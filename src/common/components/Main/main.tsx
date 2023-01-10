fc080ca40de02cfb4bca8ebb1ea5ab6172f40d91
Azimka <71658005+Azimka228@users.noreply.github.com>
2023-01-11 01:58:14 +0300

    refactor: minor code changes

--- src/common/components/Main/main.tsx
+++ src/common/components/Main/main.tsx
@@ -4,7 +4,6 @@ import Login from '../../../features/login/Login'
 import SignUp from '../../../features/register/SignUp'
 import Profile from '../../../features/profile/Profile'
 import { Navbar } from '../Navbar/navbar'
-import Error404 from '../../../features/Error404/Error404'
 import Password from '../../../features/password/Password'
 import NewPassword from '../../../features/password/newPassword/NewPassword'
 import ForgotPassword from '../../../features/password/forgotPassword/ForgotPassword'
@@ -23,6 +22,7 @@ const Main = () => {
           <>
             <Route path={'/profile'} element={<Profile />} />
             <Route path="/login" element={<Navigate to="/profile" replace />} />
+            <Route path="*" element={<Navigate to="/profile" replace />} />
           </>
         ) : (
           <>
@@ -31,9 +31,9 @@ const Main = () => {
             <Route path={'/password'} element={<Password />} />
             <Route path={'/forgot'} element={<ForgotPassword />} />
             <Route path={'/newpassword'} element={<NewPassword />} />
+            <Route path="*" element={<Navigate to="/login" replace />} />
           </>
         )}
-        <Route path="*" element={<Error404 />} />
       </Routes>
     </div>
   )
