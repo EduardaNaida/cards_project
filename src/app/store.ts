0747c4437b2092f176b583f0eb9887fc973fb381
Yau1heni <lodbrok9@gmail.com>
2023-01-10 15:59:20 +0300

    feat: profile page added

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { AnyAction, applyMiddleware, combineReducers, legacy_createStore } from 'redux'
-import thunk, { ThunkDispatch } from 'redux-thunk'
+import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { appReducer } from './appReducer'
+import { LoginAT, profileReducer } from '../features/profile/profile-reducer'
 
 const rootReducer = combineReducers({
   app: appReducer,
+  profile: profileReducer,
 })
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
@@ -12,6 +14,7 @@ export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
 export type AppDispatchType = ThunkDispatch<AppRootStateType, any, AnyAction>
+export type AppThunk<ReturnType = void> = ThunkAction<void, AppRootStateType, unknown, LoginAT>
 
 export const UseAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
 export const AppDispatch = () => useDispatch<AppDispatchType>()
---
+++ src/common/components/EditableSpan/EditableSpan.tsx
@@ -0,0 +1,47 @@
+import React, { ChangeEvent, useState } from 'react'
+import editIcon from '../../../assets/img/editIcon.svg'
+import SuperButton from '../SuperButton/SuperButton'
+import s from './EditableSpan.module.css'
+
+type EditableSpanPropsType = {
+  name: string
+  changeName: (newName: string) => void
+}
+
+const EditableSpan: React.FC<EditableSpanPropsType> = ({ name, changeName }) => {
+  const [isEditMode, setIsEditMode] = useState<boolean>(false)
+  const [newName, setNewName] = useState(name)
+
+  const onEditMode = () => {
+    setIsEditMode(true)
+  }
+  const offEditMode = () => {
+    setIsEditMode(false)
+    changeName(newName)
+  }
+  const onChangeEditHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewName(e.currentTarget.value)
+  }
+
+  const onClickEditHandler = () => {
+    offEditMode()
+  }
+
+  return (
+    <div className={s.editableSpan}>
+      {isEditMode ? (
+        <>
+          <input value={newName} autoFocus onChange={onChangeEditHandler} />
+          <SuperButton onClick={onClickEditHandler}>save</SuperButton>
+        </>
+      ) : (
+        <>
+          <span onDoubleClick={onEditMode}>{name}</span>
+          <img className={s.editIcon} onClick={onEditMode} src={editIcon} alt={'editIcon'} />
+        </>
+      )}
+    </div>
+  )
+}
+
+export default EditableSpan
